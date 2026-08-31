528e819b96a3b2cb2dbc72035ac48cfe57374c82
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-08-31 13:02:43 +0000

    Etapa de Cadastro

--- gestor_sala.py
+++ gestor_sala.py
@@ -1,1 +1,10 @@
+#Etapa1 - Cadastro da Empresa e Projetos
+startups = {
+    "nome": "Flare Solution",
+    "etapa": "Aceleração",  
+    "inicio": "Abril de 2026"
+}
+projetos_ativos =["App Mobile","Signal Found"]
 
+print("STARTUP:",startups["nome"],"Fase:",startups["etapa"])
+print("Projeto em Andamento:",projetos_ativos[0],"Inicio:",startups["inicio"])
